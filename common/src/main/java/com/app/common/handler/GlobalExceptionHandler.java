272904458eb7f49d282a6e03fd1a6a1612443589
Антон <toshabespalov2006@gmail.com>
2023-08-26 19:22:42 +0300

    exception handler

--- common/src/main/java/com/app/common/handler/GlobalExceptionHandler.java
+++ common/src/main/java/com/app/common/handler/GlobalExceptionHandler.java
@@ -5,11 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(InvalidEmailException.class)
     public ResponseEntity<Error> handleMethodArgumentNotValidException(InvalidEmailException emailException) {
@@ -20,7 +21,7 @@ public class GlobalExceptionHandler {
         return new Error(status, runtimeException.getMessage());
     }
 
-    public record Error(HttpStatus status, String message){
+    private record Error(HttpStatus status, String message){
 
     }
 }
