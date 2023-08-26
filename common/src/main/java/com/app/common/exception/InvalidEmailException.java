dc32fdbf220c2ddf634b72a0dcf505ec4d73a882
Антон <toshabespalov2006@gmail.com>
2023-08-26 18:51:18 +0300

    exception handler

--- common/src/main/java/com/app/common/exception/InvalidEmailException.java
+++ common/src/main/java/com/app/common/exception/InvalidEmailException.java
@@ -1,9 +1,5 @@
 package com.app.common.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public class InvalidEmailException extends RuntimeException{
 
     public InvalidEmailException(String message){
