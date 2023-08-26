da9a0bab081f86f398896aff874a2e972ebcc8bb
Антон <toshabespalov2006@gmail.com>
2023-08-26 18:46:49 +0300

    exception handler

--- common/src/main/java/com/app/common/constraints/ValidEmailValidator.java
+++ common/src/main/java/com/app/common/constraints/ValidEmailValidator.java
@@ -1,27 +1,17 @@
 package com.app.common.constraints;
 
+import com.app.common.exception.InvalidEmailException;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.apache.commons.validator.routines.EmailValidator;
 
 public class ValidEmailValidator implements ConstraintValidator<ValidEmail, String> {
 
-    private String messageError;
-
     @Override
     public boolean isValid(String email, ConstraintValidatorContext context) {
         if (EmailValidator.getInstance().isValid(email)) {
             return true;
         }
-        context.disableDefaultConstraintViolation();
-        context
-            .buildConstraintViolationWithTemplate(messageError)
-            .addConstraintViolation();
-        return false;
-    }
-
-    @Override
-    public void initialize(ValidEmail constraintAnnotation) {
-        messageError = constraintAnnotation.message();
+        throw new InvalidEmailException("Invalid email format");
     }
 }
