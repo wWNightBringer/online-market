ff37feb035cca6f9cb5f6bc8bccc0c5e2ee39f8d
wWNightBringer <andrey27059622@gmail.com>
2023-08-28 18:10:12 +0300

    Bucket

---
+++ order/src/main/java/com/app/order/repository/BucketRepository.java
@@ -0,0 +1,7 @@
+package com.app.order.repository;
+
+import com.app.order.domain.Bucket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BucketRepository extends JpaRepository<Bucket, Integer> {
+}
