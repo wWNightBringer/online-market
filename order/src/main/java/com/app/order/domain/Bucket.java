1c4f7e3a6793e934c68c3d654f48cbf27832f9d4
wWNightBringer <andrey27059622@gmail.com>
2023-08-31 16:17:09 +0300

    ProductBucket

--- order/src/main/java/com/app/order/domain/Bucket.java
+++ order/src/main/java/com/app/order/domain/Bucket.java
@@ -1,11 +1,26 @@
 package com.app.order.domain;
 
 import com.app.common.domain.BaseModel;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.Where;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -27,4 +42,16 @@ public class Bucket extends BaseModel {
     private int productCount;
     @Column(name = "user_id")
     private int userId;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+        name = "product_bucket",
+        joinColumns = {
+            @JoinColumn(name = "bucket_id")
+        },
+        inverseJoinColumns = {
+            @JoinColumn(name = "product_id")
+        }
+    )
+    private List<Product> products;
 }
